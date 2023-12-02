15ef66f95665485c3a2c43d43d492afcf60580f6
user543 <sreejithtmurali@gmail.com>
2023-12-02 17:09:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/taskkkk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taskkkk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<TableProvider>(create: (_) => TableProvider()),
+     ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MINTO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Simulating a delay to show the splash screen
+    Future.delayed(
+      Duration(seconds: 2),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      },
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'MINTO',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TableSelectionScreen()),
+            );
+          },
+          child: Text('Login'),
+        ),
+      ),
+    );
+  }
+}
+
+class TableSelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Table Selection'),
+      ),
+      body: Consumer<TableProvider>(
+        builder: (context, tableProvider, _) {
+         return GridView.builder(
+            itemCount: tableProvider.tables.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0
+            ),
+            itemBuilder: (BuildContext context, int index){
+              var table = tableProvider.tables[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+
+                    if (!table.isOccupied) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MenuScreen()),
+                      );
+                    } else {
+                      // Show some message indicating the table is occupied
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Table ${index + 1} is occupied!')),
+                      );
+                    }
+                  },
+                  child: Container(
+                    height: 150,
+
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/ss.jpg'),
+                        fit: BoxFit.scaleDown,
+                      ),
+                      border: Border.all(
+                        width: 0.5,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            top: 5,
+                            left: 10,
+                            child:  Text(table.isOccupied ? 'Occupied' : 'Open',style: TextStyle(color: table.isOccupied ? Colors.red : Colors.green,fontWeight: FontWeight.bold),))
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+
+
+
+
+        },
+      ),
+    );
+  }
+}
+
+
+
+class MenuScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu'),
+      ),
+      body: Center(
+        child: Text('Menu Screen'),
+      ),
+    );
+  }
+}
+
+class TableModel {
+  final int id;
+  bool isOccupied;
+
+  TableModel({required this.id, this.isOccupied = false});
+}
+class TableProvider extends ChangeNotifier {
+  List<TableModel> _tables = [TableModel(id: 1,isOccupied:true),
+    TableModel(id: 1,isOccupied:true),
+    TableModel(id: 2,isOccupied:false),
+    TableModel(id: 3,isOccupied:false),
+    TableModel(id: 4,isOccupied:true),
+    TableModel(id: 5,isOccupied:true),
+    TableModel(id: 6,isOccupied:false),
+    TableModel(id: 7,isOccupied:true)
+  ];
+
+  List<TableModel> get tables => _tables;
+
+  void occupyTable(int tableId) {
+    _tables[tableId].isOccupied = true;
+    notifyListeners();
+  }
+}
